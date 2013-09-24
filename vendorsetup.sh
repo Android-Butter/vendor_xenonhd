d2020cf604513713454fcd6fc1e6168fe139c3b3
PURE AOSP! <androidbutter@gmail.com>
2013-09-24 06:29:02 -0400

    Update vendorsetup.sh

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -5,20 +5,7 @@ add_lunch_combo xenonhd_d2usc-userdebug
 add_lunch_combo xenonhd_d2vzw-userdebug
 add_lunch_combo xenonhd_e975-userdebug
 add_lunch_combo	xenonhd_endeavoru-userdebug
-add_lunch_combo xenonhd_find5-userdebug
-add_lunch_combo xenonhd_galaxysmtd-userdebug
-add_lunch_combo xenonhd_grouper-userdebug
-add_lunch_combo xenonhd_i605-userdebug
-add_lunch_combo xenonhd_i9100-userdebug
-add_lunch_combo xenonhd_i9100g-userdebug
-add_lunch_combo xenonhd_i9300-userdebug
-add_lunch_combo xenonhd_i9305-userdebug
-add_lunch_combo xenonhd_jfltevzw-userdebug
-add_lunch_combo xenonhd_l900-userdebug
-add_lunch_combo xenonhd_m7-userdebug
-add_lunch_combo xenonhd_maguro-userdebug
-add_lunch_combo xenonhd_mako-userdebug
-add_lunch_combo xenonhd_mb886-userdebug
+
 add_lunch_combo xenonhd_n7000-userdebug
 add_lunch_combo xenonhd_n7100-userdebug
 add_lunch_combo xenonhd_t0lte-userdebug
